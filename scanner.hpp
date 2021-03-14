e174f09ec2b9374c22d8777dae6da72564acf79a
Stephen Mwangi <st3v3n.mw@gmail.com>
2021-03-14 23:50:42 +0300

    moved things around

--- scanner.hpp
+++ scanner.hpp
@@ -64,14 +64,6 @@ string scanNumber() {
     return buffer;
 }
 
-// cerr << endl << line;
-// if (line[line.size() - 1] != '\n')
-//     cerr << endl;
-// if (token_start_pos - 1 > 0)
-//     cerr << string(token_start_pos - 1, ' ');
-// cerr << string(cur_pos - token_start_pos - 1, '^') << endl;
-// cerr << "Unexpected token " << token << " on line " << line_number << endl;
-
 vector<TokenNode> scanner(const string &file_name) {
     char ch;
     vector<TokenNode> tokens = {};
@@ -243,8 +235,16 @@ vector<TokenNode> scanner(const string &file_name) {
                             tokens.push_back(TokenNode(ch == '+' ? T_PLUS : T_MINUS, ch == '+' ? "+" : "-"));
                     }
                 } else {
-                    if (!(ch == ' ' || ch == '\n' || ch == '\t' || ch == '\r'))
+                    if (!(ch == ' ' || ch == '\n' || ch == '\t' || ch == '\r')) {
+                        // cerr << endl << line;
+                        // if (line[line.size() - 1] != '\n')
+                        //     cerr << endl;
+                        // if (token_start_pos - 1 > 0)
+                        //     cerr << string(token_start_pos - 1, ' ');
+                        // cerr << string(cur_pos - token_start_pos - 1, '^') << endl;
+                        // cerr << "Unexpected token " << token << " on line " << line_number << endl;
                         cerr << "alaa -> " << ch << endl; // token not recognized
+                    }
                 }
         }
     }
