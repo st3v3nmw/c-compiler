1c81ffa80abaedfdc9780c9d69ca2b3e19ac890d
Stephen Mwangi <st3v3n.mw@gmail.com>
2021-05-25 08:31:42 +0300

    change test09

--- tests/test09.c
+++ tests/test09.c
@@ -1,5 +1,24 @@
+float q = 37/-100.0; // -0.37
+
 int main() {
-    z
-    if (true) {
+    printf("\nHello world!");
+    
+    if (53 > 89) { // false
+        printf(q); // -0.37
+    } else {
+        printf(1 + q); // 0.63
+    }
+
+    int i = 0;
+    while (i <= 3) {
+        printf(i);
+        i = i + 1;
+    }
+
+    int foo;
+    for (foo = 32; foo > 29; foo = foo - 1) {
+        printf(foo);
+    }
+
     return 0;
 }
